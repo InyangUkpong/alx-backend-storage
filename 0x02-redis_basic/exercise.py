83ef5b5f00788fa3f3ed831ce12a34c5feaf93d3
InyangUkpong <inyangweb@gmail.com>
2023-04-12 12:40:49 +0100

    Redis Basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,7 +19,7 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    '''store the history of inputs and outputs for a particular function'''
+    '''store the history of inputs and output for a particular function'''
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         '''wrap the decorated function and return the wrapper'''
